ee2156023e23670fe7be1b674ce576f6fbf36df2
Math-Soares <msv9@aluno.ifal.edu.br>
2026-03-24 10:28:11 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+import 'home_page.dart';
+
+void main() {
+  runApp(const mainApp());
+}
+
+class mainApp extends StatefulWidget {
+  const mainApp({super.key});
+
+  @override
+  State<mainApp> createState() => _mainAppState();
+}
+
+class _mainAppState extends State<mainApp> {
+  int _selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        bottomNavigationBar: NavigationBar(
+          selectedIndex: _selectedIndex,
+          onDestinationSelected: (int index) {
+            setState(() {
+              _selectedIndex = index;
+            });
+          },
+          destinations: [
+            NavigationDestination(
+              icon: Icon(Icons.home),
+              selectedIcon: Icon(Icons.home_outlined),
+              label: 'Home',
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.search),
+              selectedIcon: Icon(Icons.search_outlined),
+              label: 'Search',
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.settings),
+              selectedIcon: Icon(Icons.settings_outlined),
+              label: 'Config',
+            )
+          ],
+        ),
+        body: <Widget>[
+          homePage(),
+          Text('search page'),
+          Text('config page')
+        ][_selectedIndex],
+      ),
+    );
+  }
+}
