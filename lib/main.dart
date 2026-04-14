a5fd52d79f19269ec4600db5e1768217edae7d45
Maycon-Daniel31 <mdms3@aluno.ifal.edu.br>
2026-04-14 11:57:46 -0300

    third commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,66 @@
 import 'package:flutter/material.dart';
-
-import 'onboarding_page.dart';
+import 'package:primeiroaplicativo/config_page.dart';
+import 'package:primeiroaplicativo/onboarding_page.dart';
 
 void main() {
-  runApp(const mainApp());
+  runApp(const MainApp());
 }
 
-class mainApp extends StatefulWidget {
-  const mainApp({super.key});
+class MainApp extends StatefulWidget {
+  const MainApp({super.key});
 
   @override
-  State<mainApp> createState() => _mainAppState();
+  State<MainApp> createState() => _MainAppState();
 }
 
-class _mainAppState extends State<mainApp> {
+class _MainAppState extends State<MainApp> {
   int _selectedIndex = 0;
+  ThemeMode _themeMode = ThemeMode.light;
+
+  void _onThemeChanged(bool isDark) {
+    setState(() {
+      _themeMode = isDark ? ThemeMode.dark : ThemeMode.light;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      themeMode: _themeMode,
       home: Scaffold(
-        bottomNavigationBar: NavigationBar(
-          selectedIndex: _selectedIndex,
-          onDestinationSelected: (int index) {
-            setState(() {
-              _selectedIndex = index;
-            });
-          },
-          destinations: [
-            NavigationDestination(
-              icon: Icon(Icons.home),
-              selectedIcon: Icon(Icons.home_outlined),
-              label: 'Home',
-            ),
-            NavigationDestination(
-              icon: Icon(Icons.search),
-              selectedIcon: Icon(Icons.search_outlined),
-              label: 'Search',
-            )
-          ],
-        ),
-        body: <Widget>[
-          OnboardingPage(),
-        ][_selectedIndex],
+          bottomNavigationBar: NavigationBar(
+            selectedIndex: _selectedIndex,
+            indicatorColor: Colors.transparent,
+            onDestinationSelected: (int index) {
+              setState(() {
+                _selectedIndex = index;
+              });
+            },
+            destinations: [
+              NavigationDestination(
+                icon: Icon(Icons.location_on_outlined),
+                selectedIcon: Icon(Icons.location_on),
+                label: 'Onboarding',
+              ),
+              NavigationDestination(
+                icon: Icon(Icons.settings_outlined),
+                selectedIcon: Icon(Icons.settings),
+                label: 'Config',
+              )
+            ],
+          ),
+          body: IndexedStack(
+            index: _selectedIndex,
+            children: [
+              OnboardingPage(),
+              ConfigPage(
+                isDark: _themeMode == ThemeMode.dark,
+                onThemeChanged: _onThemeChanged,
+              )
+            ],
+          )
       ),
     );
   }
--- lib/onboarding_page.dart
+++ lib/onboarding_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class OnboardingPage extends StatefulWidget {
   const OnboardingPage({super.key});
@@ -10,6 +11,57 @@ class OnboardingPage extends StatefulWidget {
 class _OnboardingPage extends State<OnboardingPage> {
   @override
   Widget build(BuildContext context) {
-    return Text('Home Page');
+    final colorScheme = Theme.of(context).colorScheme;
+
+    return SafeArea(
+        child: Scaffold(
+          body: ListView(
+            children: [
+              Center(
+                child: Card(
+                  shape: CircleBorder(),
+                  child: Icon(Icons.location_on_outlined, size: 50),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(26),
+                child: Center(
+                  child: Text(
+                    'Usar sua localização?',
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.inter(fontSize: 28, fontWeight: FontWeight.w700,
+
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Center(
+                  child: Text(
+                    textAlign: TextAlign.center,
+                    'Permita o acesso para receber previsões automáticas da sua cidade em tempo real.',
+                    style: GoogleFonts.inter(fontSize: 13, fontWeight: FontWeight.w700),
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () => {},
+                style: ButtonStyle(foregroundColor: WidgetStatePropertyAll(colorScheme.onSurface)),
+                child: Text(
+                  'Permitir Localização'
+                ),
+              ),
+              ElevatedButton(
+                  onPressed: () => {},
+                  style: ButtonStyle(foregroundColor: WidgetStatePropertyAll(colorScheme.onSurface)),
+                  child: Text(
+                  'Inserir cidade manualmente'
+                )
+              )
+            ],
+          ),
+        )
+    );
   }
 }
